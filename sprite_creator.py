6789e1097a3838eea88fc0d56f2916e34f3801c4
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2022-12-08 18:00:03 -0600

    initial commit

---
+++ sprite_creator.py
@@ -0,0 +1,94 @@
+import math
+import os
+
+from PIL import Image, ImageFile
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+
+class Sprite(object):
+    def __init__(self, width, height, across, gap, name):
+        self.item_width = width
+        self.item_height = height
+        self.items_across = across
+        self.items_down = 1
+        self.gap = gap
+        self.sheet_width = self.item_width * self.items_across + self.gap * (self.items_across - 1)
+        self.sheet_height = self.item_height  # this will be increased as we add more images
+        self.filename = name + '.png'
+        self.sheet = None
+
+        self.current_location = -1  # we start out at -1 because we increment prior to adding anything
+        self.current_col = 0
+        self.current_row = 0
+        self.current_x = 0
+        self.current_y = 0
+
+    def open_from_file(self, location=None):
+        if location:
+            self.current_location = location
+        if not os.path.isfile(self.filename):
+            self.create_new()
+        else:
+            self.sheet = Image.open(self.filename)
+
+    def open_from_image(self, image):
+        self.sheet = image
+
+    def create_new(self):
+        self.sheet = Image.new('RGBA', (self.sheet_width, self.sheet_height))
+
+    def add_next_image_from_file(self, image_path):
+        image = Image.open(image_path)
+        self.add_next_image(image)
+
+    def add_image_at_location_from_file(self, image_path, location):
+        image = Image.open(image_path)
+        self.add_image_at_location(image, location)
+
+    def add_next_image(self, img):
+        self.increment_current_location()  # adding the default image will be handled separately
+        self.add_image(img)
+
+    def add_image(self, img):
+        if self.current_row == self.items_down:
+            self.add_row()
+        box = (self.current_x, self.current_y)
+        img = img.resize((self.item_width, self.item_height), Image.ANTIALIAS)
+        self.sheet.paste(img, box)
+
+    def add_row(self):
+        _, height = self.sheet.size
+        if self.current_y > height:
+            self.items_down += 1
+            old_sheet = self.sheet
+            self.sheet_height = self.sheet_height + self.gap + self.item_height
+            self.create_new()
+            self.sheet.paste(old_sheet, (0, 0))
+
+    def add_image_at_location(self, img, location):
+        self.update_current_location(location)
+        self.add_image(img)
+
+    def increment_current_location(self):
+        self.update_current_location(self.current_location + 1)
+
+    def update_current_location(self, new_location):
+        self.current_location = new_location
+        self.current_col = self.current_location % self.items_across
+        self.current_row = math.floor(self.current_location / self.items_across)
+        self.current_x = self.current_col * (self.item_width + self.gap)
+        self.current_y = self.current_row * (self.item_height + self.gap)
+
+    def get_slice(self, location):
+        self.update_current_location(location)
+        crop_rectangle = (
+        self.current_x, self.current_y, self.current_x + self.item_width, self.current_y + self.item_height)
+        return self.sheet.crop(crop_rectangle)
+
+    def destroy(self, location):
+        rect = Image.new('RGBA', (self.item_width, self.item_height))
+        self.add_image_at_location(rect, location)
+
+    def save(self):
+        self.sheet.save(self.filename)
---
+++ sprite_from_folder.py
@@ -0,0 +1,54 @@
+import urllib.request, time, sprite_creator, io, os
+
+SUFFIX = ''
+SPRITE_NAME = 'types'
+IMAGE_DIR = 'Sprites/' + SPRITE_NAME + ' Images'
+DATA_FILE_LOCATION = 'Sprites/' + SPRITE_NAME + 'Sprite' + SUFFIX + '.txt'
+IMAGE_WIDTH = 25
+IMAGE_HEIGHT = 25
+IMAGE_GAP = 1
+IMAGES_ACROSS = 4
+
+SPRITE_FILE_NAME = 'Sprites/' + SPRITE_NAME + 'Sprite' + SUFFIX
+
+sprite = sprite_creator.Sprite(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGES_ACROSS, IMAGE_GAP, SPRITE_FILE_NAME)
+sprite.create_new()
+
+lines = [
+    """return {{
+        settings = {{
+            align = 'middle',
+            autolinksuffix = '',
+            defaultlengthkey = 'link',
+            defaultlinkkey = 'link',
+            height = {},
+            lookup = 'SPRITENAMEnames',
+            name = 'SPRITENAME',
+            nourl = 'Yes',
+            sheetsize = {},
+            spacing = {},
+            stylesheet = true,
+            url = require( [[Module:Sprite]] ).getUrl( 'SPRITENAMESprite.png'),
+            width = {},
+        }},
+        sections = {{
+            {{ name = 'Uncategorized', id = 1 }},
+        }},
+        ids = {{""".format(str(IMAGE_HEIGHT), str(sprite.sheet_width), str(IMAGE_GAP), str(IMAGE_WIDTH)).replace(
+        'SPRITENAME', SPRITE_NAME)
+]
+
+for pos, fname in enumerate(os.listdir(IMAGE_DIR)):
+    name = fname.replace('.png', '')
+    sprite.add_next_image_from_file(IMAGE_DIR + '/' + fname)
+    lines.append('\t\t["{}"] = {{ pos = {}, section = 1 }},'.format(name.replace('_', ' '), pos + 2))
+
+lines.append('\t\t["{}"] = {{ pos = {}, section = 1 }},'.format('unknown', '1'))
+
+lines.append('	},')
+lines.append('}')
+
+with open(DATA_FILE_LOCATION, 'w', encoding="utf-8") as f:
+    f.write('\n'.join(lines))
+
+sprite.save()
